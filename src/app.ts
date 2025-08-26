c9cb5dcc4394bff81b1e4090336a1f446208ce23
Prabodh More <more.prabodh@gmail.com>
2025-08-26 14:30:06 +0530

    chore: local logger setup

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,17 @@
 import express from "express";
 
+import { logger } from './utils/logger';
+
 export function createApp() {
     const app = express();
 
     app.use(express.json());
+
+    // Request logging
+    app.use((req, _res, next) => {
+        logger.info(`${req.method} ${req.path}`, { ip: req.ip });
+        next();
+    });
 
     app.get("/", (_req, res) => {
         res.send("Hello World");
